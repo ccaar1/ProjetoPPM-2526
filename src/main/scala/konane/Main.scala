43ec9caa0f8e301e02d1fc906ca20600b8d8daf9
Cristiana Correia <ccaar1@iscte-iul.pt>
2026-05-13 16:55:29 +0100

    Last update - to make it work

--- src/main/scala/konane/Main.scala
+++ src/main/scala/konane/Main.scala
@@ -3,9 +3,22 @@ package konane
 object Main:
   def main(args: Array[String]): Unit =
     val rand = MyRandom()
-    val (board, open, r2) = Game.createGame(6, 6, rand)
+    val rows = 6
+    val cols = 6
+    val (board, open, r2) = Game.createGame(rows, cols, rand)
 
-    println(Game.boardToString(board, 6, 6))
+    println(Game.boardToString(board, rows, cols))
 
-    val (coord, r3) = Game.randomMove(open, r2)
-    println(s"Random move: $coord")
+
+    val (f1, t1) = Game.getValidMoves(board, Stone.Black, rows, cols).head match {
+      case (f, ts) => (f, ts.head)
+    }
+    val (b1, o1) = Game.play(board, Stone.Black, f1, t1, open)
+    println(s"\n--- Black move-se $f1 -> $t1 ---\n${Game.boardToString(b1.get, rows, cols)}")
+
+
+    val (f2, t2) = Game.getValidMoves(b1.get, Stone.White, rows, cols).head match {
+      case (f, ts) => (f, ts.head)
+    }
+    val (b2, o2) = Game.play(b1.get, Stone.White, f2, t2, o1)
+    println(s"\n--- White move-se $f2 -> $t2 ---\n${Game.boardToString(b2.get, rows, cols)}")
